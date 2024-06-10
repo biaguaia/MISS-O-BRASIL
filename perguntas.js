4ab3845f4ec31757ce97657f57535c1648ca8309
biaguaia <128636385+biaguaia@users.noreply.github.com>
2024-06-10 15:46:34 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,7 @@
+const perguntas = [
+    enunciado, "Pergunta 1", "Você virou presidente e para liderar e escolher o futuro do Brasil,em primeiro lugar qual das dua opçoẽs você escolheria ?",
+    alternativas, [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+];
---
+++ script.js
@@ -0,0 +1,26 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    enunciados ,  
+    "Você virou presidente e para liderar e escolher o futuro do Brasil,em primeiro lugar qual das dua opçoẽs você escolheria ?" ,
+    alternativas, [
+        "Alternativa 1",
+        "Alternativa 2"
+    ]
+,
+    "Com a resposta que você escolheu você demonstrou interesse em mudar o futuro do país quais dessas atitudes você acharia mais vantajosa para o nosso país?",
+    alternativas, [
+        "Alternativa 1",
+        "Alternativa 2"
+    ],
+    "Quanto tempo você levaria para colocar uma ação em pratica ?",
+    alternativas, [
+        "Alternativa 1",
+        "Alternativa 2"
+    ],
+    
+];
